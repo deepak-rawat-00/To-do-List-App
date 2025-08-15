a3c3dd8178e692e6401d1205c5e8907bd6bcf251
deepak-rawat-00 <155062182+deepak-rawat-00@users.noreply.github.com>
2025-08-15 22:17:52 +0530

    Fix base path for Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/To-do-List-App/",
+  base: "/", // ✅ Root path for Netlify
   plugins: [react()],
 })
+
